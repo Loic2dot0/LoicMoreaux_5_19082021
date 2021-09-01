6a1aa19ec52d9873d69892ae0d6eca530f9ba3cb
Loic2dot0 <moreaux.loic@gmail.com>
2021-09-01 13:19:33 +0200

    basket.js création de la fonction du compteur du panier. Modification du nom de l'id pour l'affichage du compteur du panier. Ajout de l'appel de la fonction du compteur du panier.

--- js/basket.js
+++ js/basket.js
@@ -1,9 +1,22 @@
 var myBasket = localStorage;
 
+
+
 function basketLength(){
     return myBasket.length;
 }
 
 function keyName(){
     return 'article' + basketLength();
+}
+
+function basketCount(){
+    let count = basketLength();
+    let displayCount = document.getElementById("basketCount");
+    if(count == 0){
+        displayCount.innerHTML = "";
+    }
+    else{
+        displayCount.innerHTML = count;
+    }
 }
