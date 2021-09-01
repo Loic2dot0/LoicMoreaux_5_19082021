847b0a0f772536e2ef7747dabb4926449324e5d3
Loic2dot0 <moreaux.loic@gmail.com>
2021-09-01 16:53:32 +0200

    basket.js: modification fonction displayBasket.

--- js/basket.js
+++ js/basket.js
@@ -30,11 +30,26 @@ function addBasket(){
 function clearBasket(){
     myBasket.clear();
     basketCount();
+    displayBasket();
 }
 
 function displayBasket(){
     if(basketLength() == 0){
-        document.getElementById("listBasket").innerHTML = '<div class="col fs-5">Votre panier est tristement vide... <b class="fs-3">:\'(</b></div>';
-        document.getElementById("btnClear").style.visibility = "hidden";
+        voidBasket();
     }
+    else{
+        for(let i = 0; i < basketLength(); i++){
+            let article = myBasket.getItem(myBasket.key(i));
+            article = JSON.parse(article);
+            console.log(myBasket.key(i));
+            console.log(article);
+        }
+    }
+}
+
+function voidBasket(){
+    document.getElementById("listBasket").innerHTML = '<div class="col fs-5">Votre panier est tristement vide... <b class="fs-3">:\'(</b></div>';
+    document.getElementById("btnClear").style.visibility = "hidden";
+    document.getElementById("btnSubmit").style.backgroundColor = "#f8f9fa";
+    document.getElementById("btnSubmit").setAttribute("disabled", "");
 }
