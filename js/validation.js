8e7109fee1438fedfbd6b4e223d1896eaf9ffbd3
Loic2dot0 <moreaux.loic@gmail.com>
2021-09-19 18:37:20 +0200

    validation.html: modification pour l'affhichage du nuléro de commande et du prix total. validation.js: ajout de la fonction d'affichage du prix total.

--- js/validation.js
+++ js/validation.js
@@ -1,12 +1,17 @@
 var myOrder = JSON.parse(myBasket.order);
 var myOrderId = myOrder.orderId;
-var myProducts = myOrder.Products;
+var myProducts = myOrder.products;
+
 console.log('Order Id : ' + myOrderId);
 console.log(myProducts);
 
-
 window.addEventListener("load", function() {
+    for(let i in myProducts){
+        totalBasket += myProducts[i].price;
+    }
+    
     myBasket.clear();
     basketCount(1);
     document.getElementById('orderId').innerHTML = myOrderId;
+    displayTotalPrice();
 });
